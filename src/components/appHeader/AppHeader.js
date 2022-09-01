fbb89fe032874a692b85f45c92bc788a37aefa7c
Волков Андрей Олегович <volkovao@sengkh.local>
2022-09-01 16:47:50 +0300

    first

---
+++ src/components/appHeader/AppHeader.js
@@ -0,0 +1,38 @@
+import { Link, NavLink } from 'react-router-dom';
+import './appHeader.scss';
+
+const AppHeader = () => {
+    return (
+        <header className='app__header'>
+            <h1 className='app__title'>
+                <Link to='/'>
+                    <span>Marvel</span> information portal
+                </Link>
+            </h1>
+            <nav className='app__menu'>
+                <ul>
+                    <li>
+                        <NavLink
+                            end
+                            style={({ isActive }) => ({ color: isActive ? '#ef0013' : 'inherit' })}
+                            to='/'
+                        >
+                            Characters
+                        </NavLink>
+                    </li>
+                    /
+                    <li>
+                        <NavLink
+                            style={({ isActive }) => ({ color: isActive ? '#ef0013' : 'inherit' })}
+                            to='/comics'
+                        >
+                            Comics
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default AppHeader;
